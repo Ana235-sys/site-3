c42c2ba6c9e20804d07c07e726e25e49f031e2a4
Ana235-sys <anamariaionescu562@gmail.com>
2024-12-10 19:44:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function openarticles(){
+    window.location.href = "exemplu.html";
+}
+function openreviews(){
+    window.location.href = " reviews.html";
+
+}
+function openmedia(){
+    window.location.href = "media.html";
+}
